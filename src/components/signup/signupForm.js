09db21d3b0382cc838b27bbcd96b73dd61ebf446
Kristmund Ryggstein <kristmunds@hotmail.com>
2020-10-23 19:02:56 +0100

    .

--- src/components/signup/signupForm.js
+++ src/components/signup/signupForm.js
@@ -57,7 +57,7 @@ const SignupForm = ({ selectedItems }) => {
     if (isSubmitted && isSubmitSuccessful && emailDraft && selectedItems.length > 0) {
       SendEmail(`${process.env.GATSBY_EMAIL_END_POINT}`, emailDraft)
       let olavursEmail = emailDraft
-      olavursEmail.to = "oe@tokni.fo"
+      olavursEmail.to = "oe@tokni.com"
       SendEmail(`${process.env.GATSBY_EMAIL_END_POINT}`, olavursEmail)
 
       navigate(`/registered`)
