f94d1aede05974edf9c851564c639e99a4f26b9a
Kristmund Ryggstein <kristmunds@hotmail.com>
2020-10-30 13:39:48 +0000

    added scroll bar

--- src/components/NewsBox.js
+++ src/components/NewsBox.js
@@ -1,3 +1,5 @@
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { graphql, useStaticQuery } from 'gatsby'
 import React, { useState } from 'react'
 import styled from 'styled-components'
@@ -23,26 +25,86 @@ query fetchNews {
   }
   `)
   const articles = data.allStrapiArticle
-  console.log("articles", data)
+  const months = [
+    `jan`,
+    `feb`,
+    `mars`,
+    `apr`,
+    `mai`,
+    `juni`,
+    `juli`,
+    `aug`,
+    `sep`,
+    `okt`,
+    `nov`,
+    `des`,
+  ]
   return (
     <ContainerStyle>
-      <TitleStyle>NÝGGJASTU TÌÐINDI</TitleStyle>
-      {articles.nodes.map((item, key) => (
-        <div></div>
-      ))}
+      <TitleStyle>
+        <div>NÝGGJASTU TÌÐINDI</div>
+        <IconStyle icon={faTimes}/>
+      </TitleStyle>
+      {articles.nodes.map((item, index) => {
+        const dateString = item.date.split("-")
+        return (
+          <NewsItemContainer key={index}>
+            <DateStyle>{dateString[0]}<div>{months[dateString[1] - 1].toUpperCase()}</div></DateStyle>
+            <div>
+              <NewsTitleStyle>{item.title}</NewsTitleStyle>
+              <Img />
+            </div>
+          </NewsItemContainer>
+        )
+      })}
     </ContainerStyle>
   )
 }
 const ContainerStyle = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
+
   background: #F5F5F5 0% 0% no-repeat padding-box;
+  height: 325px;
+  overflow-y: scroll;
+  overflow-x: hidden;
 `
 
 const TitleStyle = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  align-self: stretch;
+  margin: 15px 30px;
   font-size: 22px;
 `
 
 const NewsItemContainer = styled.div`
-
+  display: flex;
+  flex: 1;
+  align-items: center;
+  background-color: white;
+  margin: 20px;
+  height: 50px;
+  width: 90%;
+`
+const DateStyle = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 50px;
+  height: 50px;
+  border-right: 1px solid red;
 `
 
+const NewsTitleStyle = styled.div`
+  margin: 0 10px;
+`
+
+const IconStyle = styled(FontAwesomeIcon)`
+
+`
 export default NewsBox
