08d17d896a1cf0e2f3019c8ad4b261e649f0217c
Kristmund Ryggstein <kristmunds@hotmail.com>
2020-11-10 09:35:17 +0000

    shows savn title

--- src/pages/library.js
+++ src/pages/library.js
@@ -185,10 +185,8 @@ const DateStyle = styled.div`
 
 const TitleStyle = styled.h3`
   color: #58A449;
-  display: none;
   font-size: 24px;
   ${media.desktop3`
-    display: block;
     margin-top: 100px;
   `}
   `
